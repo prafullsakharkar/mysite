9c5600cfd7cf1a38c02e779b696a168f77045e5f
Prafull Sakharkar <prafullsakharkar@gmail.com>
2023-10-05 21:00:43 +0530

    Canvas Ball removed and changed some images

--- frontend/components/Tech.jsx
+++ frontend/components/Tech.jsx
@@ -1,18 +1,51 @@
 "use client";
 
 import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { fadeIn } from "@utils/motion";
 
-import BallCanvas from "@components/canvas/Ball";
+import Image from "next/image";
 import SectionWrapper from "@hoc/SectionWrapper";
 import { technologies } from "@constants";
+
+
+const TechCard = ({ index, name, icon }) => (
+  <Tilt className='xs:w-[150px] md:w-[150px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-full shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-full py-5 px-12 min-h-[150px] flex justify-evenly items-center flex-col'
+      >
+        <Image
+          width={50}
+          height={50}
+          src={icon}
+          alt='software-engineer'
+          className='object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {name}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
 
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
+      {technologies.map((technology, index) => (
+        <TechCard key={technology.name} index={index} {...technology} />
       ))}
     </div>
   );
